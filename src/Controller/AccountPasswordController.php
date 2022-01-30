6c7ebc5b89cdd5b604ede7255af0cfa47130bddb
kevin SANTANDER <kevin.santander@protonmail.ch>
2022-01-30 18:47:10 +0100

    adding easyadmin

---
+++ src/Controller/AccountPasswordController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ChangePasswordType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AccountPasswordController extends AbstractController
+{
+    private $entityManager;
+        
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    #[Route('/account/modify-password', name: 'account_password')]
+    public function index(Request $request, UserPasswordHasherInterface $hasher): Response
+    {
+
+        $notification = null;
+
+        $user = $this->getUser();
+        $form = $this->createForm( ChangePasswordType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()){
+            $old_pwd = $form->get('old_password')->getData();
+
+            if($hasher->isPasswordValid($user,$old_pwd)){
+                $new_pwd = $form->get('new_password')->getData();
+                $password = $hasher->hashPassword($user, $new_pwd);
+
+                $user->setPassword($password);
+                $this->entityManager->persist($user);
+                $this->entityManager->flush();
+                $notification = 'Votre mot de passe a été changé avec succès';
+            } else {
+                $notification = 'Votre mot de passe actuel est incorrect';
+            }
+        }
+
+        return $this->render('account/password.html.twig', [
+            'form' => $form->createView(),
+            'notification' => $notification
+        ]);
+    }
+}
